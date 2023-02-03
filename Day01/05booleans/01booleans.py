7119af7b89416cfa4814173288180d2b87b80e71
Ashish Malla <ashishmalla45@gmail.com>
2023-02-03 16:30:34 -0700

    add list of falsy values

---
+++ Day01/05booleans/01booleans.py
@@ -0,0 +1,15 @@
+None
+False
+0 #(integer)
+0.0 #(floating-point)
+"" #(empty string)
+() #(empty tuple)
+[] #(empty list)
+{} #(empty dictionary)
+set() #(empty set)
+
+# Check if an object is an integer or not:
+
+x = 200
+print(isinstance(x, int))
+
