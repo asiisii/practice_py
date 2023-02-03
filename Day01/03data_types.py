aed9d4df9df9218cfa0233b353037bad15008b92
Ashish Malla <ashishmalla45@gmail.com>
2023-02-02 22:36:27 -0700

    update notes with steps to get random number

--- Day01/03data_types.py
+++ Day01/03data_types.py
@@ -46,3 +46,13 @@ b = int(y)
 
 #convert from int to complex:
 c = complex(x)
+
+
+
+#  to generate random number
+# Python does not have a random() function to make a random number, but Python has a built-in module called random that can be used to make random numbers
+import random
+
+print(random.randrange(1, 10))
+
+
