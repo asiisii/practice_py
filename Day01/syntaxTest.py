688c65cc2cc9e68580364a2ddf6aac2497eb7c8d
Ashish Malla <ashishmalla45@gmail.com>
2023-02-02 22:04:31 -0700

    notes on indentation and comments

---
+++ Day01/syntaxTest.py
@@ -0,0 +1,15 @@
+if 5 < 2:
+  print('5 is greater than 2') #indentation matters
+if 5 > 2:
+  print('5 is smaller than 2')
+
+
+# comments
+
+# single line comments
+'''
+line1
+line2
+''' #multiline comments
+
+
