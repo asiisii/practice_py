36d7f777a378ba1e6e97d62db4086b733a457485
Ashish Malla <ashishmalla45@gmail.com>
2023-02-02 22:29:17 -0700

    add notes on data types

---
+++ Day01/03data_types.py
@@ -0,0 +1,30 @@
+#  str
+x = "Hello World"		
+# int
+x = 20
+# float 
+x = 20.5
+# complex	
+x = 1j
+# list	
+x = ["apple", "banana", "cherry"]
+# tuple	
+x = ("apple", "banana", "cherry")
+# range	
+x = range(6)
+# dict	
+x = {"name" : "John", "age" : 36}	
+# set
+x = {"apple", "banana", "cherry"}	
+# frozenset
+x = frozenset({"apple", "banana", "cherry"})
+# bool	
+x = True
+# bytes	
+x = b"Hello"
+# bytearray	
+x = bytearray(5)
+# memoryview	
+x = memoryview(bytes(5))
+# NoneType	
+x = None	
