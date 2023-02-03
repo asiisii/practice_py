3431d9749e390aa450d7be5325fe2d7b94b494b7
Ashish Malla <ashishmalla45@gmail.com>
2023-02-02 23:34:01 -0700

    remove

---
+++ Day01/04strings.py
@@ -0,0 +1,36 @@
+# can assign a multiline string to a variable by using three quotes
+a = '''Lorem ipsum dolor sit amet,
+consectetur adipiscing elit,
+sed do eiusmod tempor incididunt
+ut labore et dolore magna aliqua.'''
+
+
+# string are arrays
+x = 'hello'
+x[0] #this will return 'h'
+
+#  looping through string 
+
+for x in "banana":
+  print(x)
+
+#  use len() method to get length of the string
+
+
+# Check if "free" is present in the following text:
+
+txt = "The best things in life are free!"
+print("free" in txt)
+
+
+# Print only if "free" is present:
+
+txt = "The best things in life are free!"
+if "free" in txt:
+  print("Yes, 'free' is present.")
+
+
+# Check if "expensive" is NOT present in the following text:
+
+txt = "The best things in life are free!"
+print("expensive" not in txt)
