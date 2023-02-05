89d5b674fa6d0cdfbe989f19f15abfcef9dfc982
Ashish Malla <ashishmalla45@gmail.com>
2023-02-04 21:38:29 -0700

    add notes on accessing list items

---
+++ Day01/07dataCollections/01lists/01list.py
@@ -0,0 +1,15 @@
+# Syntax 
+['one', 1, True, False, -1]
+
+# can use the list() constructor to create list
+mylist = list((1,2,True, False, "Hello"))
+# [1, 2, True, False, 'Hello']
+
+
+# Python - Access List Items
+mylist[1]  # 2
+mylist[-1] # 'Hello'
+mylist[:3] # [1,2, True] 
+mylist[1:] # [2, True, False, 'Hello']
+
+# 
