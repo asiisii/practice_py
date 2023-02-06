566aac2ff7dd046fc262cb257cf8da8b059a1250
Ashish Malla <ashishmalla45@gmail.com>
2023-02-05 23:49:11 -0700

    add a trick to update tuple indirectly

--- Day01/07dataCollections/02tuples/01tuple.py
+++ Day01/07dataCollections/02tuples/01tuple.py
@@ -23,3 +23,25 @@ print(some_elements) # prints (2, 3)
 for element in my_tuple:
     print(element)
 
+
+# Python - Update Tuples
+
+# convert the tuple to a list, 
+# modify the elements of the list, 
+# and then convert the list back to a tuple
+
+# create a tuple
+my_tuple = (1, 2, 3, 4, 5)
+
+# convert the tuple to a list
+my_list = list(my_tuple)
+
+# modify the elements of the list
+my_list[0] = 10
+my_list[-1] = 50
+
+# convert the list back to a tuple
+my_tuple = tuple(my_list)
+print(my_tuple) # prints (10, 2, 3, 4, 50)
+
+
