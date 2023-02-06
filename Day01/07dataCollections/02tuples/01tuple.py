74588d22ece2a64f0d8ebf023737ae96081eab1e
Ashish Malla <ashishmalla45@gmail.com>
2023-02-05 23:45:17 -0700

    add notes on accessing elements

---
+++ Day01/07dataCollections/02tuples/01tuple.py
@@ -0,0 +1,25 @@
+# Syntax
+# create a tuple
+my_tuple = (1, 2, 3, 4, 5)
+
+# Ordered, Unchangeable, allows duplicates
+
+# Python - Access Tuple Items
+# same as list
+
+# access the first element
+first_element = my_tuple[0]
+print(first_element) # prints 1
+
+# access the last element
+last_element = my_tuple[-1]
+print(last_element) # prints 5
+
+# access a slice of elements
+some_elements = my_tuple[1:3]
+print(some_elements) # prints (2, 3)
+
+# iterate over the elements
+for element in my_tuple:
+    print(element)
+
