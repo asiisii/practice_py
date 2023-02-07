70b41d4a494b67ece5af839b028c11ead85e10dd
Ashish Malla <ashishmalla45@gmail.com>
2023-02-06 23:19:15 -0700

    add notes on loops

--- Day01/07dataCollections/04dictionary/01dictionary.py
+++ Day01/07dataCollections/04dictionary/01dictionary.py
@@ -45,3 +45,12 @@ del person #will remove the entire variable
 person.clear() # person will equal to {} now
 
 
+# Python - Loop Dictionaries
+for key, value in person.items():
+  print(key, value)
+
+for key in person.keys():
+  print(key)
+
+for value in person.values():
+  print(value)
