4549ac149697cb42b5195043315a86fdfaa45633
Ashish Malla <ashishmalla45@gmail.com>
2023-02-07 23:53:14 -0700

    add notes on __init__ and self

--- Day01/013classes&objects/01classes&objects.py
+++ Day01/013classes&objects/01classes&objects.py
@@ -16,3 +16,25 @@ print(p1.name) # John
 print(p1.age) # 36
 
 
+# another example
+
+class Dog:
+    species = "Canis familiaris"
+
+    def __init__(self, name, breed, age):
+        self.name = name
+        self.breed = breed
+        self.age = age
+
+    def bark(self):
+        print(f"{self.name} barks!")
+
+dog = Dog("Fido", "Labrador", 3)
+
+print(dog.name)  # Output: Fido
+dog.bark()  # Output: Fido barks!
+
+
+# The __init__ method is called a constructor and is automatically called 
+# when an object of the class is created. 
+# self is a reference to the instance of the class and is used to access its attributes and methods.
