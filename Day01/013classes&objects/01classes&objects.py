e4d2c68d17f2297781127ef3c216858c6cca323f
Ashish Malla <ashishmalla45@gmail.com>
2023-02-08 00:03:01 -0700

    add notes on why and when we need to use pass  in class

--- Day01/013classes&objects/01classes&objects.py
+++ Day01/013classes&objects/01classes&objects.py
@@ -47,3 +47,10 @@ dog.bark()  # Output: Fido barks!
 # The __str__ method is used to provide a human-readable representation of an object. 
 # It is called when you use the built-in str() function or the print function on an object, 
 # and it should return a string that represents the object
+
+
+
+# class definitions cannot be empty, but if you for some reason have a class definition with no content, 
+# put in the pass statement to avoid getting an error.
+class Person:
+  pass
