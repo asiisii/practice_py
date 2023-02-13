5b4c63ed8646660e569064db8fdac904316f1e9c
Ashish Malla <ashishmalla45@gmail.com>
2023-02-13 09:34:30 +0545

    add noes on math

---
+++ Day01/019math/01math.py
@@ -0,0 +1,31 @@
+# Python Math
+
+#  min and max method
+x = min(5, 10, 25)
+y = max(5, 10, 25)
+print(x) # 5
+print(y) # 25
+
+#  abs 
+x = abs(-7.25)
+print(x) # 7.25
+
+
+x = pow(4, 3) #same as 4 * 4 * 4
+print(x) # 64
+
+
+import math
+
+x = math.sqrt(64)
+print(x) # 8.0
+
+x = math.ceil(1.4)
+y = math.floor(1.4)
+
+print(x) # returns 2
+print(y) # returns 1
+
+x = math.pi
+
+print(x) # 3.141592653589793
